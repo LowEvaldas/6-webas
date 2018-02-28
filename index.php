885353355fd8032abee212e328347899e4c46570
DESKTOP-NFPHQ3P\Vartotojas <lowevaldas@gmail.com>
2018-02-28 22:05:47 +0200

    Picture. 4 images below.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
             <img class="img-responsive" src="img/bigimg.jpg"></img>
         </div>
     </div>
-<div class="articles">
+    <div class="articles">
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6">
@@ -31,6 +31,12 @@
             </div>
         </div>
     </div>
-
+    <div class="container"><div class="line"></div></div>
+    <div class="text-betw">
+        <div class="container">
+            <span>“ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non diam erat. In fringilla massa ut nisi ullamcorper pellentesque „</span>
+        </div>
+    </div>
+    <div class="container"><div class="line"></div></div>
 </div>
 <?php include 'footer.php' ?>
