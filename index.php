eafe93b43d90a54f6a060c53b965bccad2a5a207
DESKTOP-NFPHQ3P\Vartotojas <lowevaldas@gmail.com>
2018-03-01 13:04:22 +0200

    Paskutiniai pakeitimai namuose.

--- index.php
+++ index.php
@@ -38,5 +38,53 @@
         </div>
     </div>
     <div class="container"><div class="line"></div></div>
+
+    <div class="projects">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <img class="img-responsive center-block" src="img/img.jpg"></img>
+                    <h1>Projektas</h1>
+                    <p>Portortornec condimenterdum eget consectetuer condis consequam pretium pellus sed mauris enim. Puruselit mauris nulla hendimentesque elit semper nam a sapien urna sempus.</p>
+                    <div class="more clearfix">
+                        <div class="more-fl">
+                            <a href="#"><span>View Details »</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <img class="img-responsive" src="img/img2.jpg"></img>
+                    <h1>Project Title</h1>
+                    <p>Portortornec condimenterdum eget consectetuer condis consequam pretium pellus sed mauris enim. Puruselit mauris nulla hendimentesque elit semper nam a sapien urna sempus.</p>
+                    <div class="more clearfix">
+                        <div class="more-fl">
+                            <a href="#"><span>View Details »</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <img class="img-responsive" src="img/img3.jpg"></img>
+                    <h1>Project Title</h1>
+                    <p>Portortornec condimenterdum eget consectetuer condis consequam pretium pellus sed mauris enim. Puruselit mauris nulla hendimentesque elit semper nam a sapien urna sempus.</p>
+                    <div class="more clearfix">
+                        <div class="more-fl">
+                            <a href="#"><span>View Details »</span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <img class="img-responsive" src="img/img4.jpg"></img>
+                    <h1>Project Title</h1>
+                    <p>Portortornec condimenterdum eget consectetuer condis consequam pretium pellus sed mauris enim. Puruselit mauris nulla hendimentesque elit semper nam a sapien urna sempus.</p>
+                    <div class="more clearfix">
+                        <div class="more-fl">
+                            <a href="#"><span>View Details »</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 <?php include 'footer.php' ?>
